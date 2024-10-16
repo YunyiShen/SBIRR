39430dabde5f4130f3a4bf32a2eb9da820a24c97
Yunyi Shen <yshen99@outlook.com>
2024-10-16 15:54:49 -0400

    names

--- package/setup.py
+++ package/setup.py
@@ -5,6 +5,6 @@ from setuptools import setup
 setup(name='SBIRR',
       version='0.0.1',
       description='Schrodinger Bridge with Iterative Reference Refinement',
-      author='SOMEONE',
+      author='Yunyi Shen and Renato Berlinghieri',
       packages=[],
      )
